42689ef0de26a06f0bc39a49182b8f7f26981c8d
Subham Sharma <rssubham63@gmail.com>
2026-01-13 18:45:02 +0530

    Rectified Responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 function Card({logoUrl,heading,text,imageUrl}) {
   return (
     <div className="card">
-        <img src={logoUrl} alt={heading} className="logo" height="60" width="60"/>
-        <div className="text">
-            <h2>{heading} <br /></h2>
-            <hr />
-            <p>{text}</p>
+        <div className="text-container">
+            <img src={logoUrl} alt={heading} className="logo" height="60" width="60"/>
+            <div className="text">
+                <h2>{heading} <br /></h2>
+                <hr />
+                <p>{text}</p>
+            </div>
         </div>
         <div className="image">
             <img src={imageUrl} alt={heading} height="80%" width="150px" />
